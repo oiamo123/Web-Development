cefa767ed41bc685cd3f13abb08c5a69ac11f459
Gavin <oiamo123@gmail.com>
2024-05-16 14:50:25 -0600

    Tailwind + Sign-in page

---
+++ Tailwind/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/*.html/'],
+  theme: {
+    extend: {
+      scale: {
+        '200': '2'
+      }
+    },
+  },
+  plugins: [],
+}
+
